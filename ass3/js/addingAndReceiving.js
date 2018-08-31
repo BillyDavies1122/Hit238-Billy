688dbd0543f32f0b1a23ae1091ab447fc05793c7
BillyDavies1122 <36976243+BillyDavies1122@users.noreply.github.com>
2018-09-01 02:12:04 +0930

    changed syntax

--- ass3/js/addingAndReceiving.js
+++ ass3/js/addingAndReceiving.js
@@ -1,25 +1,29 @@
-//key generator function obtained from https://gist.github.com/gordonbrander/2230317
-//created by user gordon brander
+
 document.getElementById('addButton').addEventListener('click',
-function(event) {
-  event.preventDefault();
-  var value = {
-    "date":document.getElementById('inputDate').value,
-    "name":document.getElementById('inputName').value,
-    "sets":document.getElementById('inputSets').value,
-    "reps":document.getElementById('inputReps').value
-  };
-  var key = function () {
-    // Math.random should be unique because of its seeding algorithm.
-    // Convert it to base 36 (numbers + letters), and grab the first 9 characters
-    // after the decimal.
-  return '_' + Math.random().toString(36).substr(2, 9);
-  };
-  console.log('test')
-  sessionStorage.setItem(key, value);
-})
+  function(event) {
+    event.preventDefault();
+    var value = {
+      "date":document.getElementById('inputDate').value,
+      "name":document.getElementById('inputName').value,
+      "sets":document.getElementById('inputSets').value,
+      "reps":document.getElementById('inputReps').value
+    };
+
+  var key = keyGenerator()
+    console.log('test')
+    sessionStorage.setItem(key, value);
+  })
 
 
 function listOfDb(){
 
 }
+
+//key generator function obtained from https://gist.github.com/gordonbrander/2230317
+//created by user gordon brander
+var keyGenerator = function () {
+  // Math.random should be unique because of its seeding algorithm.
+  // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+  // after the decimal.
+  return '_' + Math.random().toString(36).substr(2, 9);
+};
